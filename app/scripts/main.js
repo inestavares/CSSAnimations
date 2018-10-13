4cb14d13dfb7d2cbeb48eee96430d541c214d8d5
Robin Schroer <sulami@peerwire.org>
2018-10-13 18:48:35 +0200

    Fix the bouncing ball

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,35 +4,21 @@ $(window).load(function() {
 	// create a function animate items that takes a parameter i
 		// declare variables
 		var animatable = $('.animatable');
-		var animationClasses = ["fadein", "drawwidth", "slideleft", "fadein", "fadein", "drawwidth", "fadein"]; 
+		var animationClasses = ['fadein', 'drawwidth', 'slideleft', 'fadein', 'fadein', 'drawwidth', 'fadein'];
 		var preAnimationEnd = 'animationend webkitAnimationEnd MozTransitionEnd oTransitionEnd';
 		var i = 0;
 		$(animatable[0]).addClass('fadein').toggleClass('invisible');
 			$(animatable).on(preAnimationEnd, function() {
-				console.log("meh");
+				console.log('meh');
 				i++;
 				$(animatable[i]).addClass(animationClasses[i]).toggleClass('invisible');
 			});
-			
-		$('.firstarch').on(preAnimationEnd, function() {
-			$('#ball-container').addClass('secondarch').on(preAnimationEnd, function() {
-				$('#ball-container').addClass('thirdarch').on(preAnimationEnd, function() {
-					$('#ball-container').addClass('fourtharch');
+		$('.firstbounce').on(preAnimationEnd, function() {
+			$('#ball').addClass('secondbounce').on(preAnimationEnd, function() {
+				$('#ball').addClass('thirdbounce').on(preAnimationEnd, function() {
+				    $('#ball').addClass('fourthbounce');
 					/* Act on the event */
-				});;
-
+				});
 			});
-		});		
-
-
-			
-	});
-
-
-		
-		
-		
-
-
-
-
+		});
+});
